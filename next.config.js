e597c5ef3e687568b7e8dc6a1db1fa3545dd6fce
Flickinny11 <Logantbaird@gmail.com>
2025-05-15 13:57:30 -0500

    Add next.config.js 1747335449856

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -17,11 +19,23 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'www.apple.com',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
     ],
   },
-  experimental: {
-    serverActions: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+      'components': path.resolve(__dirname, 'src/components'),
+      'context': path.resolve(__dirname, 'src/context'),
+      'lib': path.resolve(__dirname, 'src/lib'),
+    }
+    return config
   },
 }
 

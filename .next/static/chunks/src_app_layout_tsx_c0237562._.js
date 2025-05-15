a31cdf8f6f3d7980e6d7e7b9ad5b02392f93916b
Flickinny11 <Logantbaird@gmail.com>
2025-05-15 13:59:13 -0500

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1747335553513

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_dist_6d0b70ed._.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm2017_5f0ce35e._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+  "static/chunks/node_modules_framer-motion_dist_es_ad8b9672._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_c7a0b6ce._.js",
+  "static/chunks/node_modules_bd573575._.js",
+  "static/chunks/src_967dc55e._.js"
+],
+    source: "dynamic"
+});
